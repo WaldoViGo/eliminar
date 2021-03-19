3f221d8b5775ee52ed3cab620135090f425adb93
WaldoViGo <62714104+WaldoViGo@users.noreply.github.com>
2021-03-19 10:41:52 -0400

    Add files via upload

---
+++ TesPosTDD/src/tespostdd/NotaApr.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tespostdd;
+
+/**
+ *
+ * @author Wart
+ */
+public class NotaApr {
+    public int Aprobar(int i){
+     if (i > 50){
+         return 1;
+        //System.out.println("aprobbado...");
+     }else
+         //System.out.println("failed....");     
+        return 0;
+    }
+}
